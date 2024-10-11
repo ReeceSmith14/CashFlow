7bd591addb91b35d15db75795cfca45ab6a5d8b5
Reece Smith <reecesmith@hotmail.co.uk>
2024-10-11 20:48:42 +0000

    feat: google sign in added

--- cashflow/cashflow/settings.py
+++ cashflow/cashflow/settings.py
@@ -153,40 +153,5 @@ AUTHENTICATION_BACKENDS = [
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        # For each OAuth-based provider, either add a ``SocialApp``
-        # (``socialaccount`` app) containing the required client
-        # credentials, or list them here:
-        'APP': {
-            'client_id': '123',
-            'secret': '456',
-            'key': ''
-        }
-    },
-    'apple': {
-        'APPS': [{
-            # Your service identifier.
-            'client_id': 'your.service.id',
-
-            # The Key ID (visible in the "View Key Details" page).
-            'secret': 'KEYID',
-
-            # Member ID/App ID Prefix -- you can find it below your name
-            # at the top right corner of the page, or it’s your App ID
-            # Prefix in your App ID.
-            'key': 'MEMAPPIDPREFIX',
-
-            'settings': {
-                # The certificate you downloaded when generating the key.
-                'certificate_key': """-----BEGIN PRIVATE KEY-----
-s3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr
-3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3cr3ts3
-c3ts3cr3t
------END PRIVATE KEY-----"""
-            }
-        }]
-    }
-}
 
 
